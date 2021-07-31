6f1677cf12a67d564081194c486c2c0b7315b881
shashi2712 <68730526+shashi2712@users.noreply.github.com>
2021-07-31 11:13:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+// //import React from 'react'
+
+// const App = () => {
+//   return (
+//     <>
+//       <MyName/>
+//       <h1 className = "myclassname">Hello World</h1>
+//       <p>This is my second react project.</p>
+      
+//     </>
+//   );
+// };
+// const MyName = () =>{
+//   return(
+//     <h1>Shashi Bhushan Jha</h1>
+//   )
+// };
+
+// export default App
+import React from 'react'
+import Restaurant from './component/Basics/Restaurant';
+
+const App = () => {
+  return (
+    <Restaurant/>
+  )
+}
+
+export default App
+
